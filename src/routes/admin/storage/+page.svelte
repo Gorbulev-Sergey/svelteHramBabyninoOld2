cab5adba2695646e0cf3bd3ac5701eeb3543ad06
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-09-29 00:12:24 +0300

    storage tabs

--- src/routes/admin/storage/+page.svelte
+++ src/routes/admin/storage/+page.svelte
@@ -8,6 +8,7 @@
 	let folders = [];
 	let photos = [];
 	let photosForUpload = [];
+	let tabs = { selectedTab: 0, tab: ['Хранилище фотографий', 'Хранилище документов'] };
 
 	// Корневая папка для фотографий
 	let albumsFolder = '/Photos';
@@ -32,115 +33,143 @@
 	});
 </script>
 
-<PageTitle title="Папки с фотографиями">
-	<div slot="navigation">
-		<div class=" input-group">
-			<div class="input-group-text border-0">Создать папку:</div>
-			<input class="form-control bg-light text-dark border-0" bind:value={newFolderName} placeholder="имя новой папки" />
-			<button class="btn btn-light text-dark" on:click={() => (newFolderName = '')}><i class="fa-solid fa-delete-left" /></button>
-			<button
-				class="btn btn-dark"
-				on:click={() => {
-					// Пустой объект для создания новой папки
-					const empty = ref(storage, `${albumsFolder}/${newFolderName}/empty`);
-					// Создаём папку и записываем в неё пустой объект
-					uploadString(empty, 'Строка для инициализации папки').then(s => {
-						newFolderName = '';
-						getFolders();
-					});
-				}}>Создать</button>
-		</div>
+<nav class="d-flex">
+	<h4 class="flex-grow-1 text-uppercase px-1 my-0">{tabs.tab[tabs.selectedTab]}</h4>
+	<div class="nav d-flex flex-wrap mb-4">
+		<a
+			class="flex-grow-1 btn btn-light text-dark border-0 rounded-0 rounded-start py-2 active"
+			data-bs-toggle="tab"
+			href="#photos"
+			on:click={() => (tabs.selectedTab = 0)}>Фотографии</a>
+		<a
+			class="flex-grow-1 btn btn-light text-dark rounded-0 rounded-end border-0 py-2"
+			data-bs-toggle="tab"
+			href="#documents"
+			on:click={() => (tabs.selectedTab = 1)}>Документы</a>
 	</div>
-</PageTitle>
+</nav>
 
-<div class="d-flex flex-wrap justify-content-start align-items-start gap-2 mb-5">
-	{#each folders as item, i}
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<div
-			class="d-flex align-items-center {selectedFolder == i ? 'bg-dark text-light' : 'bg-light'}  px-2 rounded-1"
-			style=" cursor:pointer"
-			on:click={() => {
-				selectedFolder = i;
-				getPhotos();
-			}}>
-			<div class="p-2">{item.name}</div>
-			<button
-				class="btn btn-sm {selectedFolder == i ? 'btn-dark' : 'btn-light'} text-danger"
-				on:click|stopPropagation={() => {
-					listAll(ref(storage, `/${item.fullPath}`)).then(s => {
-						if (s.items.filter(p => p.name != 'empty').length == 0) {
-							deleteObject(ref(storage, `${item.fullPath}/empty`)).then(() => getFolders());
-						}
-					});
-				}}><i class="fa-solid fa-xmark" /></button>
+<div class="tab-content">
+	<div id="photos" class="tab-pane fade show active">
+		<PageTitle title="Папки с фотографиями">
+			<div slot="navigation">
+				<div class=" input-group">
+					<div class="input-group-text border-0">Создать папку:</div>
+					<input class="form-control bg-light text-dark border-0" bind:value={newFolderName} placeholder="имя новой папки" />
+					<button class="btn btn-light text-dark" on:click={() => (newFolderName = '')}
+						><i class="fa-solid fa-delete-left" /></button>
+					<button
+						class="btn btn-dark"
+						on:click={() => {
+							// Пустой объект для создания новой папки
+							const empty = ref(storage, `${albumsFolder}/${newFolderName}/empty`);
+							// Создаём папку и записываем в неё пустой объект
+							uploadString(empty, 'Строка для инициализации папки').then(s => {
+								newFolderName = '';
+								getFolders();
+							});
+						}}>Создать</button>
+				</div>
+			</div>
+		</PageTitle>
+
+		<div class="d-flex flex-wrap justify-content-start align-items-start gap-2 mb-5">
+			{#each folders as item, i}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<div
+					class="d-flex align-items-center {selectedFolder == i ? 'bg-dark text-light' : 'bg-light'}  px-2 rounded-1"
+					style=" cursor:pointer"
+					on:click={() => {
+						selectedFolder = i;
+						getPhotos();
+					}}>
+					<div class="p-2">{item.name}</div>
+					<button
+						class="btn btn-sm {selectedFolder == i ? 'btn-dark' : 'btn-light'} text-danger"
+						on:click|stopPropagation={() => {
+							listAll(ref(storage, `/${item.fullPath}`)).then(s => {
+								if (s.items.filter(p => p.name != 'empty').length == 0) {
+									deleteObject(ref(storage, `${item.fullPath}/empty`)).then(() => getFolders());
+								}
+							});
+						}}><i class="fa-solid fa-xmark" /></button>
+				</div>
+			{/each}
 		</div>
-	{/each}
+		<PageTitle title={`Фотографии в папке "${folders[selectedFolder]?.name ?? ''}"`}>
+			<div slot="navigation">
+				<div class="input-group">
+					<button
+						class="btn btn-light border-0"
+						on:click={() => {
+							document.querySelector('#buttonSelectPhotos').click();
+						}}>{labelFakeForUploadButton}</button>
+					<input
+						id="buttonSelectPhotos"
+						hidden
+						type="file"
+						accept="image/png, image/jpeg"
+						multiple
+						class="form-control bg-light text-dark border-0"
+						bind:files={photosForUpload}
+						on:change={() =>
+							(labelFakeForUploadButton =
+								photosForUpload.length > 1 ? `Выбрано ${photosForUpload.length} фотографий` : photosForUpload[0].name)} />
+					<button
+						class="btn btn-dark"
+						on:click={() => {
+							console.log(photosForUpload);
+							Array.from(photosForUpload).forEach((p, i, a) => {
+								uploadBytes(
+									ref(storage, `/${albumsFolder}/${folders[selectedFolder].name}/${p.name.replace('(', '').replace(')', '')}`),
+									p
+								).then(() => {
+									if (i == a.length - 1) {
+										getPhotos();
+									}
+								});
+							});
+							photosForUpload = [];
+							labelFakeForUploadButton = 'Выберите фотографии для загрузки';
+						}}>Загрузить</button>
+				</div>
+			</div>
+		</PageTitle>
+		{#if photos.length > 0}
+			<div class="d-flex flex-wrap gap-2">
+				{#each photos as photo}
+					{#await getDownloadURL(ref(storage, photo.fullPath)) then s}
+						<div
+							class="d-flex align-items-start justify-content-end rounded-1"
+							style="width:23%; background-image: url({s}); background-repeat: no-repeat; background-position: center; background-size: cover; min-height:12em;">
+							<button
+								class="btn btn-sm btn-light bg-light bg-opacity-25 m-1 border-0 text-dark"
+								title="Скопировать url"
+								on:click={() => {
+									navigator.clipboard.writeText(s).then(s => {});
+								}}><i class="fa-solid fa-copy" /></button>
+							<button
+								class="btn btn-sm btn-light bg-light bg-opacity-25 m-1 border-0 text-danger"
+								title="Удалить фото"
+								on:click={() => {
+									deleteObject(ref(storage, `/${albumsFolder}/${folders[selectedFolder].name}/${photo.name}`));
+									getPhotos();
+								}}>
+								<i class="fa-solid fa-xmark" />
+							</button>
+						</div>
+					{/await}
+				{/each}
+			</div>
+		{/if}
+	</div>
+	<div id="documents" class="tab-pane fade">ddfgh</div>
 </div>
 
-<PageTitle title={`Фотографии в папке "${folders[selectedFolder]?.name ?? ''}"`}>
-	<div slot="navigation">
-		<div class="input-group">
-			<button
-				class="btn btn-light border-0"
-				on:click={() => {
-					document.querySelector('#buttonSelectPhotos').click();
-				}}>{labelFakeForUploadButton}</button>
-			<input
-				id="buttonSelectPhotos"
-				hidden
-				type="file"
-				accept="image/png, image/jpeg"
-				multiple
-				class="form-control bg-light text-dark border-0"
-				bind:files={photosForUpload}
-				on:change={() =>
-					(labelFakeForUploadButton =
-						photosForUpload.length > 1 ? `Выбрано ${photosForUpload.length} фотографий` : photosForUpload[0].name)} />
-			<button
-				class="btn btn-dark"
-				on:click={() => {
-					console.log(photosForUpload);
-					Array.from(photosForUpload).forEach((p, i, a) => {
-						uploadBytes(
-							ref(storage, `/${albumsFolder}/${folders[selectedFolder].name}/${p.name.replace('(', '').replace(')', '')}`),
-							p
-						).then(() => {
-							if (i == a.length - 1) {
-								getPhotos();
-							}
-						});
-					});
-					photosForUpload = [];
-					labelFakeForUploadButton = 'Выберите фотографии для загрузки';
-				}}>Загрузить</button>
-		</div>
-	</div>
-</PageTitle>
-{#if photos.length > 0}
-	<div class="d-flex flex-wrap gap-2">
-		{#each photos as photo}
-			{#await getDownloadURL(ref(storage, photo.fullPath)) then s}
-				<div
-					class="d-flex align-items-start justify-content-end rounded-1"
-					style="width:23%; background-image: url({s}); background-repeat: no-repeat; background-position: center; background-size: cover; min-height:12em;">
-					<button
-						class="btn btn-sm btn-light bg-light bg-opacity-25 m-1 border-0 text-dark"
-						title="Скопировать url"
-						on:click={() => {
-							navigator.clipboard.writeText(s).then(s => {});
-						}}><i class="fa-solid fa-copy" /></button>
-					<button
-						class="btn btn-sm btn-light bg-light bg-opacity-25 m-1 border-0 text-danger"
-						title="Удалить фото"
-						on:click={() => {
-							deleteObject(ref(storage, `/${albumsFolder}/${folders[selectedFolder].name}/${photo.name}`));
-							getPhotos();
-						}}>
-						<i class="fa-solid fa-xmark" />
-					</button>
-				</div>
-			{/await}
-		{/each}
-	</div>
-{/if}
+<style>
+	a.active {
+		background: var(--bs-dark);
+		color: var(--bs-light) !important;
+	}
+</style>
