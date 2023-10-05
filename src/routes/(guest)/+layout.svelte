9ad28923ee301d835e06b135015d365d9e13d378
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-10-05 23:35:39 +0300

    Revert "Merge branch 'kuksha-lesnoy' into main"
    This reverts commit 5772c999530c730577530fc4cc7429ac02473222, reversing
    changes made to 7c9b988190f4c348d5fb4e9ba7188de96aa1d831.

--- src/routes/(guest)/+layout.svelte
+++ src/routes/(guest)/+layout.svelte
@@ -12,13 +12,14 @@
 			title: 'Расписание'
 		},
 		{
+			url: '/posts/св. писание',
+			title: 'Святое Писание'
+		},
+		{
 			url: '/posts/о нашем храме',
 			title: 'О храме'
 		},
-		{
-			url: '/posts/о святом',
-			title: 'О святом'
-		},
+
 		{
 			url: '/posts/видео',
 			title: 'Видео'
@@ -40,7 +41,7 @@
 	];
 </script>
 
-<Navigation title="Храм <span class='text-danger'>КУКШИ ПЕЧЕРСКОГО</span>" {routesLeft} {routesRight} />
+<Navigation title="<span class='text-danger'>Храм</span> ВОЗНЕСЕНИЯ ГОСПОДНЯ" {routesLeft} {routesRight} />
 <div class="container my-4 py-1" style=" min-height:90vh;">
 	<slot />
 </div>
